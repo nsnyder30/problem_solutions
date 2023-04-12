2614341696cb2d52a6734044db532e12dc151797
Neil Snyder <nsnyder30@gatech.edu>
2023-04-12 09:19:21 -0700

    Commit all local changes (removed test code)

--- typescript/remove_element/remove_element.js
+++ typescript/remove_element/remove_element.js
@@ -3,7 +3,7 @@
 //  - Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
 //  - Return k.
 function removeElement(nums, val) {
-    for (var i = nums.length; i >= 0; i--) {
+    for (var i = nums.length - 1; i >= 0; i--) {
         if (nums[i] == val) {
             nums.splice(i, 1);
         }
@@ -14,15 +14,6 @@ function removeElement(nums, val) {
 var nums = [0, 1, 2, 2, 3, 0, 4, 2];
 var val = 2;
 var expectedNums = [0, 1, 4, 0, 3];
-var test = ['a', 'c', 'd'];
-console.log(test);
-test.splice(1, 1);
-console.log(test);
 console.log('nums before: ' + JSON.stringify(nums));
 var k = removeElement(nums, val); // Calls your implementation
 console.log('nums after: ' + JSON.stringify(nums));
-console.log(k == expectedNums.length);
-nums.sort(function (a, b) { return a < b ? -1 : a == b ? 0 : 1; });
-for (var i = 0; i < nums.length; i++) {
-    console.log({ ni: nums[i], en: expectedNums[i], eq: nums[i] == expectedNums[i] });
-}
